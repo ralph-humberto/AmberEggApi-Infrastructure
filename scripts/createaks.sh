ea724b0bae2a33d29b0db858c60f5386935f0be6
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-22 20:15:02 -0300

    fix pipelines

--- scripts/createaks.sh
+++ scripts/createaks.sh
@@ -3,32 +3,27 @@
 # Initialize variables used on script
 #
 echo "Initializing variables to be used on script"
-
 resourceGroupName=$1
 resourceKubernetesName=$2
-resourceCount=$3
-resourceTags=$4
-vnetId=$7
-appId=$5
-password=$6
-vnetSubnetId=$7
+resourceTags=$3
+resourceLocation=$4
+subscription=$5
+appId=$6
+password=$7
+vnetId=$8
+vnetSubnetId=$9
 
 echo "$kuberneteServiceName: check if exists"
 kuberneteServiceCheck=`az aks list --query "[?name=='$resourceKubernetesName']"`
 
 if [ ${#kuberneteServiceCheck} -lt 3 ]; then
     echo "Creating resource-kubernetes named $resourceKubernetesName with tags $resourceTags"
-	echo "    az aks create --resource-group $resourceGroupName \\\n" +
-	  	 "--location eastus \\\n" +
-         "--name $resourceKubernetesName --node-count $resourceCount \\\n" +
-         "--enable-addons monitoring --generate-ssh-keys \\\n" +
-         "--tags $resourceTags --network-plugin azure --vnet-subnet-id $vnetSubnetId \\\n" +
-         "--service-principal $appId --client-secret $password"
     az aks create --resource-group $resourceGroupName \
 		--location eastus \
-        --name $resourceKubernetesName --node-count $resourceCount \
+        --name $resourceKubernetesName --node-count 1 \
         --enable-addons monitoring --generate-ssh-keys \
-        --tags $resourceTags --network-plugin azure \
+        --tags $resourceTags --network-plugin kubenet --enable-managed-identity \
+		--vnet-subnet-id "/subscriptions/$subscription/resourceGroups/$resourceGroupName/providers/Microsoft.Network/virtualNetworks/$vnetId/subnets/$vnetSubnetId" \
         --service-principal $appId --client-secret $password
     echo "Resource kubernetes created successfully"
 else

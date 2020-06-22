a4d055a2e94accea5b6e6e149ee95550587a45aa
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-22 20:23:25 -0300

    fix pipelines

--- scripts/createaks.sh
+++ scripts/createaks.sh
@@ -20,12 +20,22 @@ if [ ${#kuberneteServiceCheck} -lt 3 ]; then
     echo "Creating resource-kubernetes named $resourceKubernetesName with tags $resourceTags"
     az aks create --resource-group $resourceGroupName \
 		--location eastus \
+		--docker-bridge-address 172.17.0.1/16 \
+        --dns-service-ip 10.0.0.10 \
+        --service-cidr 10.0.0.0/16 \
         --name $resourceKubernetesName --node-count 1 \
         --enable-addons monitoring --generate-ssh-keys \
-        --tags $resourceTags --network-plugin kubenet --enable-managed-identity \
+        --tags $resourceTags --network-plugin azure --enable-managed-identity \
 		--vnet-subnet-id "/subscriptions/$subscription/resourceGroups/$resourceGroupName/providers/Microsoft.Network/virtualNetworks/$vnetId/subnets/$vnetSubnetId" \
         --service-principal $appId --client-secret $password
     echo "Resource kubernetes created successfully"
 else
     echo "$kuberneteServiceName: already exists"
 fi
+
+Serviço CIDR
+10.0.0.0/16
+IP do serviço DNS
+10.0.0.10
+Ponte Docker CIDR
+172.17.0.1/16

6db6e30c5092bbf1dc078e61d9de2830f02eae7a
Ralph Silveira Humberto <ralph.humberto@gmail.com>
2020-06-23 06:55:14 -0300

    fix script

--- scripts/createaks.sh
+++ scripts/createaks.sh
@@ -20,14 +20,9 @@ if [ ${#kuberneteServiceCheck} -lt 3 ]; then
     echo "Creating resource-kubernetes named $resourceKubernetesName with tags $resourceTags"
     az aks create --resource-group $resourceGroupName \
 		--location $resourceLocation \
-		--docker-bridge-address 172.17.0.1/16 \
-		--load-balancer-sku standard \
-		--enable-private-cluster \
-        --dns-service-ip 10.0.0.10 \
-        --service-cidr 10.0.0.0/16 \
         --name $resourceKubernetesName --node-count 1 \
         --enable-addons monitoring --generate-ssh-keys \
-        --tags $resourceTags --network-plugin kubenet --enable-managed-identity \
+        --tags $resourceTags --network-plugin azure \
         --service-principal $appId --client-secret $password
 		#--vnet-subnet-id "/subscriptions/$subscription/resourceGroups/$resourceGroupName/providers/Microsoft.Network/virtualNetworks/$vnetId/subnets/$vnetSubnetId" \
 
